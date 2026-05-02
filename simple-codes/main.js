aff0e73bcb21cd3370512b50051ad96a498f87c1
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-03 01:11:50 +0500

    refactor: comment out unused helpers module and update pet logging

--- simple-codes/main.js
+++ simple-codes/main.js
@@ -1,4 +1,8 @@
-const helpers = module.require('./helpers');
+// const helpers = module.require('./helpers');
 
-console.log(helpers.name);
-helpers.sayHello();
+// console.log(helpers.name);
+// helpers.sayHello();
+const { myPets } = require('./myPets');
+const { cat, dog } = myPets;
+
+console.log(`My cat's name is ${cat} and my dog's name is ${dog}`);
