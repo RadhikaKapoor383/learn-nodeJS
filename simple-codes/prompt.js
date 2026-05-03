dada7cbe5c153db1b2de85e3d75804198365c0a1
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-03 18:52:28 +0500

    add prompt module

--- simple-codes/prompt.js
+++ simple-codes/prompt.js
@@ -1,9 +1,10 @@
-var prompt = module.require('prompt');
+var prompt = require('prompt');
 
 prompt.start();
 
-prompt.get(['username', 'email'], function (err, result) {
+prompt.get(['name', 'username', 'email'], function (err, result) {
     console.log('Command-line input received:');
+    console.log('  name: ' + result.name);
     console.log('  username: ' + result.username);
     console.log('  email: ' + result.email);
 });
