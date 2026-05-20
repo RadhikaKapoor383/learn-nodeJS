a5db20695f8eded3c03395aa51f3a43c08575530
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-21 03:47:25 +0500

    refactor request logging to use parsed URL and enhance log entries

--- simple-codes/http-sever/myServer.js
+++ simple-codes/http-sever/myServer.js
@@ -1,18 +1,20 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     const log = `${Date.now()}  ${req.url}  New Req received.\n`;
     const logPath = path.join(__dirname, "log.txt");
-
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl)
     fs.appendFile(logPath, log, (err) => {
         if (err) {
             res.writeHead(500, { "Content-Type": "text/plain" });
             res.end("Server error.");
             return;
         }
-        switch(req.url){
+        switch(myUrl.pathname){
             case "/":
                 res.end("Hello from server.");
                 break;
