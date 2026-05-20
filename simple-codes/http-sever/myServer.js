d97fe3a594054eca372c605d5b810218de5b7169
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-21 01:17:02 +0500

    update request logging to include URL in log entries

--- simple-codes/http-sever/myServer.js
+++ simple-codes/http-sever/myServer.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}  New Req received.\n`;
+    const log = `${Date.now()}  ${req.url}  New Req received.\n`;
     const logPath = path.join(__dirname, "log.txt");
 
     fs.appendFile(logPath, log, (err) => {
@@ -12,8 +12,17 @@ const myServer = http.createServer((req, res) => {
             res.end("Server error.");
             return;
         }
-
-        res.end("Hello from server.");
+        switch(req.url){
+            case "/":
+                res.end("Hello from server.");
+                break;
+            case "/about":
+                res.end("Response From Radhika Kapoor");
+                break;
+            default:
+                res.end("404 Not found");
+                break;
+        }
     })
 });
 myServer.listen(3004, () => console.log('Server at http://localhost:3004'))
