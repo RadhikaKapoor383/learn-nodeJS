a891551c1906a9a2da4365a4939bd0aa2ee90b7f
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-21 01:06:51 +0500

    fix logging mechanism in HTTP server and update log file path

--- simple-codes/http-sever/myServer.js
+++ simple-codes/http-sever/myServer.js
@@ -1,10 +1,19 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}  New Req recevied.\n`;
-    fs.appendFile('log.txt', log, (err, data) => {
+    const log = `${Date.now()}  New Req received.\n`;
+    const logPath = path.join(__dirname, "log.txt");
+
+    fs.appendFile(logPath, log, (err) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Server error.");
+            return;
+        }
+
         res.end("Hello from server.");
     })
-})
-myServer.listen(3000, () => console.log('Server at http://localhost:3000'))
+});
+myServer.listen(3004, () => console.log('Server at http://localhost:3004'))
