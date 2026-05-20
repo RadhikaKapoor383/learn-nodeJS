9da29527d483ef70343cbf121b258ffa7da0e744
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-21 04:23:10 +0500

    create my first express code

--- Express/index.js
+++ Express/index.js
@@ -1,2 +1,16 @@
-const http = require("http");
+// const http = require("http");
 const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) =>{
+    return res.send("Hello from Radhika");
+});
+app.get("/about", (req, res) => {
+    console.log(req.query);
+    // return res.send("Hello, This is about page." +"\n"+ req.query.name + " And age is: "+ req.query.age)
+    return res.send(`Hello, ${req.query.name}`)
+})
+
+// const myServer = http.createServer(app);
+app.listen(8000, () => console.log("Server Listening...."));
