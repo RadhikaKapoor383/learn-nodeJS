c0adbe4609e0e7cfac5d40fa772c705c3716fe89
Radhika kapoor <radhikakapoor383@gmail.com>
2026-07-09 02:50:34 +0500

    add other functions too

--- basic-yt-codes/01-Hello-world/main.js
+++ basic-yt-codes/01-Hello-world/main.js
@@ -1,3 +1,7 @@
 const math = require("./math");
 const num1 = 1, num2 = 3;
-console.log(`Addition of ${num1} and ${num2}`, math.add(num1, num2));
+console.log(`Addition of ${num1} and ${num2}:`, math.add(num1, num2));
+console.log(`Subtraction of ${num1} and ${num2}:`, math.subtract(num1, num2));
+console.log(`Multiplication of ${num1} and ${num2}:`, math.multiply(num1, num2));
+console.log(`Division of ${num1} and ${num2}:`, math.divide(num1, num2));
+console.log(`Modulus of ${num1} and ${num2}:`, math.moduler(num1, num2));
