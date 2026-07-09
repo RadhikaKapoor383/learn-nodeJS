d8161c8d4244ae76ff034ab0cc7e0ecb35d24896
Radhika kapoor <radhikakapoor383@gmail.com>
2026-07-09 22:41:20 +0500

    learning and working on file system

---
+++ basic-yt-codes/02-File-system/file.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+// Create a new file and write data to it
+// fs.writeFileSync('./text.txt', 'Heyy World!');
+
+// fs.writeFile('./text.txt', 'Heyy World! from async', (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log('Async File created successfully!');  
+//     }
+// });
+// console.log('File created successfully!');
+
+
+// read file from contact.txt
+
+// fs.readFile('./contacts.txt', 'utf-8', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(data);
+//     }
+// });
+
+
+// const resultFromReadSync = fs.readFileSync('./contacts.txt', 'utf-8');
+// console.log(resultFromReadSync);
+// console.log('File read successfully from Sync!');
+
+fs.appendFile('./contacts.txt', '\nNeha kapoor +922200000', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log('Data appended successfully!');
+    }
+});
+fs.appendFileSync('./text.txt', '\nThis is a new line added to the file using appendFileSync method.');
+console.log('Data appended successfully using appendFileSync method!');
