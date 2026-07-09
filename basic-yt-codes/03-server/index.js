413c605b41255d6f64c81f7867429afe83574389
Radhika kapoor <radhikakapoor383@gmail.com>
2026-07-10 01:34:47 +0500

    log file and different request

--- basic-yt-codes/03-server/index.js
+++ basic-yt-codes/03-server/index.js
@@ -1,9 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(`Received request: ${req.method} ${req.url}`);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, from Server!\n');
+    fs.appendFile('server.log', `${new Date().toISOString()}: ${req.url} New request received - ${req.method} ${req.url}\n`, (err) => {
+        if (err) {
+            console.error('Error writing to log file:', err);
+        }
+
+    });
+    switch (req.url) {
+        case '/':
+            res.end('Welcome to the Home Page!\n');
+            break;
+        case '/about':
+            res.end('Welcome to the About Page!\n');
+            break;
+        case '/contact':
+            res.end('Welcome to the Contact Page!\n');
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('404 Not Found\n');
+    }
 });
 
 const PORT = 8000;
